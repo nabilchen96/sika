30abfc3994923cd2db6a80d3cb1bd1161ad1bdd7
nabilchen96 <escendol900@gmail.com>
2021-05-17 15:14:08 +0700

    perbaikan checkrole

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkRole' => \app\Http\Middleware\CheckRole::class,
+        'checkRole' => \App\Http\Middleware\checkRole::class,
     ];
     
 }
