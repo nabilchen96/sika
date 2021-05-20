ee8ab3b17b3a8b3f8b5455e0e6ba04617bd6ecfb
nabilchen96 <escendol900@gmail.com>
2021-05-20 10:34:36 +0700

    pembuatan frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -145,11 +145,11 @@
       <div class="card-body">
         <div class="row no-gutters align-items-center">
           <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Hukuman Belum Selesai</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $hukuman }}</div>
+            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Lowongan Tersedia</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
           </div>
           <div class="col-auto">
-            <i class="fas fa-tasks fa-2x text-gray-300"></i>
+            <i class="fas fa-briefcase fa-2x text-gray-300"></i>
           </div>
         </div>
       </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,139 +2,272 @@
 <html lang="en">
 
 <head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Sistem Informasi Ketarunaan</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="{{ asset('template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
-  <link rel="icon" href="{{asset('logopoltekbang.png')}}">
-  <!-- Custom styles for this template-->
-  <link href="{{ asset('template/css/sb-admin-2.min.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-  @stack('style')
-
+  <title>Simple landing page</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('frontend/vendors/owl-carousel/css/owl.carousel.min.css') }} ">
+  <link rel="stylesheet" href="{{ asset('frontend/vendors/owl-carousel/css/owl.theme.default.css') }} ">
+  <link rel="stylesheet" href="{{ asset('frontend/vendors/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('frontend/vendors/aos/css/aos.css') }}">
+  <link rel="stylesheet" href="{{ asset('frontend/css/style.min.css') }}">
   <style>
-    .gambarlatar {
-      background-image: url('{{ asset("poltekbang.jpg") }}');
-      background-size: cover
+    .contact-us .contact-us-bgimage {
+      padding: 20px !important;
+      border-radius: 15px;
     }
 
-    @media only screen and (max-width: 600px) {
-      .gambarlatar{
-        display: none;
-      }
+    .card.card-body {
+      padding: 0;
+    }
+
+    .features-overview .content-header {
+      padding: 0;
+    }
+
+    .navbar {
+      padding: 18px 0;
+    }
+
+    .font-weight-semibold {
+      padding-top: 50px;
     }
   </style>
-
 </head>
 
-<body id="page-top">
-  <div class="d-flex flex-wrap align-items-stretch">
-    <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
-      <div class="p-4 m-3">
-        <img src="{{ asset('logosika.png') }}" alt="logo" width="150" class="shadow-light rounded-circle mb-5 mt-2">
-        <h2 class="text-dark font-weight-normal mb-4"><span class="font-weight-bold">Sistem Informasi Ketarunaan</span>
-        </h2>
-        {{-- <p class="text-muted">Before you get started, you must login or register if you don't already have an account.</p> --}}
-        <form method="POST" action="{{ route('login')}}" class="needs-validation mt-4" novalidate="">
-          @csrf
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-              value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-
-          <div class="form-group">
-            <div class="d-block">
-              <label for="password" class="control-label">Password</label>
-            </div>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-              name="password" required autocomplete="current-password">
-
-            @error('password')
-            <span class="invalid-feedback" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-
-          <div class="form-group">
-            <div class="custom-control custom-checkbox">
-              <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                {{ old('remember') ? 'checked' : '' }}>
-
-              <label class="form-check-label" for="remember">
-                {{ __('Remember Me') }}
-              </label>
-            </div>
-          </div>
-
-          <div class="form-group text-right">
-            {{-- <a href="auth-forgot-password.html" class="float-left mt-3">
-                  Forgot Password?
-                </a> --}}
-            <button type="submit" class="btn btn-primary btn-lg btn-icon icon-right" tabindex="4">
-              Login
-            </button>
-          </div>
-
-          <div class="mt-5 text-center">
-            {{-- Don't have an account? <a href="auth-register.html">Create new one</a> --}}
-          </div>
-        </form>
-        <br>
-        <div class="text-center mt-5 text-small">
-          Copyright &copy; Politeknik Penerbangan Palembang. Theme made with 💙 by Stisla, redesigned by Mustechs
-          {{-- <div class="mt-2">
-                <a href="#">Privacy Policy</a>
-                <div class="bullet"></div>
-                <a href="#">Terms of Service</a>
-              </div> --}}
+<body id="body" data-spy="scroll" data-target=".navbar" data-offset="100">
+  <header id="header-section">
+    <nav class="navbar navbar-expand-lg pl-3 pl-sm-0" id="navbar">
+      <div class="container">
+        <div class="navbar-brand-wrapper d-flex w-100">
+          <img src="{{ asset('frontend/images/logo.png') }}" style="width: 20%;" alt="">
+          <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="mdi mdi-menu navbar-toggler-icon"></span>
+          </button>
+          <p style="font-size: 14px; margin-top: 10px;">
+            <strong> Unit Ketarunaan</strong>
+          <br>Poltekbang Palembang</p>
+        </div>
+        <div class="collapse navbar-collapse navbar-menu-wrapper" id="navbarSupportedContent">
+          <ul class="navbar-nav align-items-lg-center align-items-start ml-auto right">
+            <li class="d-flex align-items-center justify-content-between pl-4 pl-lg-0">
+              <div class="navbar-collapse-logo">
+                <img src="{{ asset('frontend/images/Group2.svg') }}" alt="">
+              </div>
+              <button class="navbar-toggler close-button" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="mdi mdi-close navbar-toggler-icon pl-5"></span>
+              </button>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pengumuman.html">Pengumuman</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="prestasi.html">Prestasi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pelanggaran.html">Pelanggaran</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="tentang.html">Tentang</a>
+            </li>
+            <li class="nav-item btn-contact-us pl-4 pl-lg-0" style="margin-left: 20px;">
+              <button class="btn btn-info" data-toggle="modal" data-target="#exampleModal">
+                <span class="mdi mdi-lock"></span> Login</button>
+            </li>
+          </ul>
         </div>
       </div>
+    </nav>
+  </header>
+  <div class="banner">
+    <div class="container">
+      <h1 class="font-weight-semibold">Sistem Informasi <br> Ketarunaan & Alumni</h1>
+      <!-- <h6 class="font-weight-normal text-muted pb-3">Simple is a simple template with a creative design that solves all your marketing and SEO queries.</h6> -->
+      <!-- <div>
+        <button class="btn btn-opacity-light mr-1">Get started</button>
+        <button class="btn btn-opacity-success ml-1">Learn more</button>
+      </div> -->
+      <img src="{{ asset('frontend/images/undrawx.png') }}" alt="" class="img-fluid">
+      <br><br>
     </div>
-    <div
-      class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom gambarlatar">
-      <div class="absolute-bottom-left index-2">
-        <div class="text-light p-5 pb-2">
-          <div class="mb-5 pb-3">
-            <h1 class="mb-2 display-4 font-weight-bold"></h1>
-            <h5 class="font-weight-normal text-muted-transparent"></h5>
+  </div>
+  <div class="content-wrapper">
+    <div class="container">
+
+      <section class="features-overview" id="features-section">
+        <div class="content-header">
+          <h2>Pengumuman</h2>
+          <h6 class="section-subtitle text-muted">One theme that serves as an easy-to-use operational toolkit<br>that
+            meets customer's needs.</h6>
+        </div>
+        <div class="d-md-flex justify-content-between">
+          <div class="grid-margin d-flex justify-content-start">
+            <div class=>
+              <div class="card">
+                <div class="card-body">
+                  <img src="{{ asset('frontend/images/empty.jpg') }}" width="100%" alt="" class="img-fluid mb-3" style="border-radius: 15px;">
+                  <h5 class="card-title">Lowongan Kerja</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                  <a href="#" class="btn btn-primary">Detail</a>
+                </div>
+              </div>
+            </div>
           </div>
-          {{-- Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a> --}}
+          <div class="grid-margin d-flex justify-content-start">
+            <div class=>
+              <div class="card">
+                <div class="card-body">
+                  <img src="{{ asset('frontend/images/empty.jpg') }}" width="100%" alt="" class="img-fluid mb-3" style="border-radius: 15px;">
+                  <h5 class="card-title">Pengambilan Ijazah</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                  <a href="#" class="btn btn-primary">Detail</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="grid-margin d-flex justify-content-start">
+            <div class=>
+              <div class="card">
+                <div class="card-body">
+                  <img src="{{ asset('frontend/images/empty.jpg') }}" width="100%" alt="" class="img-fluid mb-3" style="border-radius: 15px;">
+                  <h5 class="card-title">Beasiswa Ketarunaan</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                  <a href="#" class="btn btn-primary">Detail</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="contact-us" id="contact-section">
+        <div class="contact-us-bgimage grid-margin">
+          <div class="row">
+            <div class="col p-12">
+              <iframe class="embed-responsive"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15938.708954153537!2d104.6991992!3d-2.9089414!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5e411b86b9a1b4e9!2sPoliteknik%20Penerbangan%20Palembang!5e0!3m2!1sid!2sid!4v1613966893900!5m2!1sid!2sid"
+                height="420" title="poltekbangplg" style="border:0" allowfullscreen></iframe>
+            </div>
+            <div class="col" style="text-align: left;">
+              <h1>Sistem Informasi Ketarunaan dan Alumni</h1>
+              <br>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae fugiat aspernatur quasi totam
+                voluptas quidem, ipsum veritatis obcaecati harum! Laudantium minima culpa cupiditate natus, distinctio
+                accusantium animi voluptatum dolores. Autem. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Blanditiis iure aperiam unde velit id distinctio! Deleniti accusantium perferendis sequi consequuntur
+                nihil! Non temporibus dignissimos tempora, asperiores et molestias amet quisquam!</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="contact-details" id="contact-details-section">
+        <div class="row text-center text-md-left">
+          <div class="col-12 col-md-6 col-lg-3 grid-margin">
+            <img src="{{ asset('frontend/images/logo.png') }}" width="30%" alt="" class="pb-2">
+            <div class="pt-2">
+              <p class="text-muted m-0">Jl. Adi Sucipto No.3012, Sukodadi, Kec. Sukarami, Palembang, Sumatera Selatan, 30961</p>
+              <p class="text-muted m-0">Email: info@poltekbangplg.ac.id</p>
+              <p class="text-muted m-0">Telpon: 0711-410930</p>
+              <p class="text-muted m-0">Fax: 0711-420385</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3 grid-margin">
+            <h5 class="pb-2">Sosial Media</h5>
+            <!-- <p class="text-muted">518 Schmeler Neck<br>Bartlett. Illinois</p> -->
+            <div class="d-flex justify-content-center justify-content-md-start">
+              <a href="#"><span class="mdi mdi-facebook"></span></a>
+              <a href="#"><span class="mdi mdi-twitter"></span></a>
+              <a href="#"><span class="mdi mdi-instagram"></span></a>
+              <a href="#"><span class="mdi mdi-youtube-play"></span></a>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3 grid-margin">
+            <h5 class="pb-2">Akses Akademik</h5>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">Sistem Informasi Akademik</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">Feeder Dikti</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">Sister Dikti</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">Learning Management System</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1">Library Management System</p>
+            </a>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3 grid-margin">
+            <h5 class="pb-2">Akses Aplikasi Lain</h5>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">Sistem Informasi Kepegawaian</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">E-persuratan</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">Skemaraja</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1 pb-2">E-marketing</p>
+            </a>
+            <a href="#">
+              <p class="m-0 pt-1">Sistem Penjamin Mutu Internal</p>
+            </a>
+          </div>
+        </div>
+      </section>
+      <footer class="border-top">
+        <p class="text-center text-muted pt-4">© Copyright Subbag Aktar Politeknik Penerbangan Palembang. Designed by<a
+            href="https://www.bootstrapdash.com/" class="px-1">Bootstrapdash.</a>All rights reserved.</p>
+      </footer>
+      <!-- Modal for Contact - us Button -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title" id="exampleModalLabel">Contact Us</h4>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="Name">Name</label>
+                  <input type="text" class="form-control" id="Name" placeholder="Name">
+                </div>
+                <div class="form-group">
+                  <label for="Email">Email</label>
+                  <input type="email" class="form-control" id="Email-1" placeholder="Email">
+                </div>
+                <div class="form-group">
+                  <label for="Message">Message</label>
+                  <textarea class="form-control" id="Message" placeholder="Enter your Message"></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-success">Submit</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="{{ asset('template/vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="{{ asset('template/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-  <script src="{{ asset('template/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="{{ asset('template/js/sb-admin-2.min.js') }}"></script>
-  @stack('scripts')
+  <script src="{{ asset('frontend/vendors/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('frontend/vendors/bootstrap/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('frontend/vendors/owl-carousel/js/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('frontend/vendors/aos/js/aos.js') }}"></script>
+  <script src="{{ asset('frontend/js/landingpage.js') }}"></script>
 </body>
 
 </html>
