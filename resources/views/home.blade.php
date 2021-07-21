fe2007254bc941f9851b8438d8b004065eed332c
nabilchen96 <escendol900@gmail.com>
2021-07-21 09:03:35 +0700

    perbaikan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
   </div>
 
   <!-- Earnings (Monthly) Card Example -->
+  @if (auth::user()->role != 'taruna')
   <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
       <div class="card-body">
@@ -44,6 +45,7 @@
       </div>
     </div>
   </div>
+  @endif
 
   <!-- Earnings (Monthly) Card Example -->
   <div class="col-xl-3 col-md-6 mb-4">
